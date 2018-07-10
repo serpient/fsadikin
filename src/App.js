b9505f0be585894c66017fad0491deea11f2ccd4
serpient <littlegangwolf@gmail.com>
2018-07-10 15:52:09 -0700

    update routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import LandingPage from './LandingPage/LandingPage';
 import Projects from './Projects/Projects';
@@ -16,21 +16,17 @@ class App extends React.Component {
   }
   render() {
     return (
-      <div className="App">
-        <Router>
-          <React.Fragment>
+        <div className="App">
             <Header />
             <Switch>
-              <Route exact={true} path="/" component={LandingPage} />
-              <Route exact={true} path="/projects" component={Projects} />
+              <Route exact={true} path={'/'} component={LandingPage} />
+              <Route exact={true} path={'/projects'} component={Projects} />
               <Route exact={true} path="/projects/Project-Match" component={ProjectMatch} />
               <Route exact={true} path="/projects/turtleTab" component={TurtleTab} />
-              <Route exact={true} path="*" component={ErrorPage} />
+              <Route path="/*" component={ErrorPage} />
             </Switch>
             <Footer />
-          </React.Fragment>
-        </Router>
-      </div>
+        </div>
     );
   }
 }
