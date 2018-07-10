59864a27936ac5c154304543836427091a66e1b4
serpient <littlegangwolf@gmail.com>
2018-07-10 12:37:52 -0700

    create project data

---
+++ src/utils/projects.js
@@ -0,0 +1,23 @@
+const projectMatch = {
+    name: 'Project Match',
+    tech: ['MongoDB', 'Express', 'React', 'NodeJS', 'TypeScript', 'Redux'],
+    description:    `Project Match is a web app created with the MERN stack with typescript integration. 
+                    It's primary function is to help programmers and designers match with projects they would be interested in. 
+                    Project Match will then provide team collaboration tools to support new teams with features such as a 
+                    dedicated platform for mockup reviews/redlines and group chat.`,
+    liveLink: 'https://www.projectmatch.me',
+    githubLink: 'https://github.com/ProjectMatch/ProjectMatch'
+}
+
+const turtleTab = {
+    name: 'TurtleTab',
+    tech: ['React', 'Redux'],
+    description:    `TurtleTab is a Google Chrome Extension Built with React. It creates a new homepage which features current 
+                    Weather, Todo and Notes functionality. It also accesses your browser data to see Bookmarks, 
+                    enable/disable Apps and Extensions, and see/clear your History. It is a collaborative effort 
+                    by a remote team of aspiring developers who met on Chingu, an international community of coders.`,
+    liveLink: 'https://chrome.google.com/webstore/detail/turtletab/imaanfkbpecmdapjnoonjjbofnfplcbd/reviews?hl=en',
+    githubLink: 'https://github.com/chingu-coders/Voyage2-Turtles-11'
+}
+
+export const projects = [projectMatch, turtleTab];
