5eaeef0d2638309a80e7fe02be277ee2e52c4d87
serpient <littlegangwolf@gmail.com>
2018-07-10 12:37:29 -0700

    error page styling

--- src/ErrorPage/ErrorPage.jsx
+++ src/ErrorPage/ErrorPage.jsx
@@ -1,14 +1,15 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 import './ErrorPage.css';
 
 class ErrorPage extends React.Component {
     render() {
         return (
             <div className="error-page-container">
-                <img className="error-image" alt="404" src={'../assets/404-03.png'} />
-                <a href="https://www.projectmatch.me" className="back-btn">
-                    <button className="error-goBack-btn">Back to Home</button>
-                </a>
+                <img className="error-image" alt="404" src={require('../assets/404-03.png')} />
+                <div className="back-btn">
+                    <Link to="/" className="error-goBack-btn">Back to Home</Link>
+                </div>
             </div>
         )
     }
