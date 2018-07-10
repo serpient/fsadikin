ff0a376ecc1159a62a627f96f271bd764f83db79
serpient <littlegangwolf@gmail.com>
2018-07-10 15:53:00 -0700

    prelim project component creator

--- src/Projects/Projects.jsx
+++ src/Projects/Projects.jsx
@@ -1,10 +1,33 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 import './Projects.css';
-
+import { projects } from '../utils/projects';
 class Projects extends React.Component {
+    skillCreator = (techArray) => {
+        return techArray.map(skill => {
+            return (
+                <div className="project-tech-skill">{skill}</div>
+            )
+        })
+    }
+    projectCreator = () => {
+        return projects.map(project => {
+            return (
+                <div className="project">
+                    <div className="project-title">{project.name}</div>
+                    <div className="project-tech">{this.skillCreator(project.tech)}</div>
+                    <div className="project-description">{project.description}</div>
+                    <Link to={'/' + project.name.replace(/ /g, '-')} className="btn-more">More Details</Link>
+                </div>
+            );
+        })
+    }
     render() {
         return (
-            <div>
+            <div className="projects-container">
+                <div className="project">
+                    {this.projectCreator()}
+                </div>
             </div>
         )
     }
