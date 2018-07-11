df1a825545144b971c064953553b01eee91487bc
serpient <littlegangwolf@gmail.com>
2018-07-10 17:36:54 -0700

    projects page content and styling

--- src/Projects/Projects.jsx
+++ src/Projects/Projects.jsx
@@ -13,19 +13,27 @@ class Projects extends React.Component {
     projectCreator = () => {
         return projects.map(project => {
             return (
-                <div className="project">
-                    <div className="project-title">{project.name}</div>
-                    <div className="project-tech">{this.skillCreator(project.tech)}</div>
-                    <div className="project-description">{project.description}</div>
-                    <Link to={'/' + project.name.replace(/ /g, '-')} className="btn-more">More Details</Link>
-                </div>
+                <React.Fragment>
+                    <div className="project-timeline-dot" />
+                    <div className="project-timeline-text">{project.date}</div>
+                    <div className="project">
+                        <div className="project-info">
+                            <div className="project-title">{project.name}</div>
+                            <div className="project-tech">{this.skillCreator(project.tech)}</div>
+                            <div className="project-description">{project.description}</div>
+                            <Link to={'/projects/' + project.name.replace(/ /g, '-')} className="btn-more">More Details</Link>
+                        </div>
+                        <img className="project-coverPhoto" src={project.coverPhoto} alt="coverPhoto" />
+                    </div>
+                </React.Fragment>
             );
         })
     }
     render() {
         return (
             <div className="projects-container">
-                <div className="project">
+                <div className="projects-container-title">Projects</div>
+                <div className="projects projects-border">
                     {this.projectCreator()}
                 </div>
             </div>
