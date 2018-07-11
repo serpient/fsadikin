ee9599fdd8236c0aca52569f1654807ddd71ba6f
serpient <littlegangwolf@gmail.com>
2018-07-10 19:17:03 -0700

    import skilLCreator fn and update reference

--- src/Projects/Projects.jsx
+++ src/Projects/Projects.jsx
@@ -2,14 +2,9 @@ import * as React from 'react';
 import { Link } from 'react-router-dom';
 import './Projects.css';
 import { projects } from '../utils/projects';
+import { skillCreator } from '../utils/skillCreator';
 class Projects extends React.Component {
-    skillCreator = (techArray) => {
-        return techArray.map(skill => {
-            return (
-                <div className="project-tech-skill">{skill}</div>
-            )
-        })
-    }
+    
     projectCreator = () => {
         return projects.map(project => {
             return (
@@ -19,7 +14,7 @@ class Projects extends React.Component {
                     <div className="project">
                         <div className="project-info">
                             <div className="project-title">{project.name}</div>
-                            <div className="project-tech">{this.skillCreator(project.tech)}</div>
+                            <div className="project-tech">{skillCreator(project.tech)}</div>
                             <div className="project-description">{project.description}</div>
                             <Link to={'/projects/' + project.name.replace(/ /g, '-')} className="btn-more">More Details</Link>
                         </div>
