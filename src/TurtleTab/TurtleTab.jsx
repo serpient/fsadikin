406a706ae0ece094a01bf0249a283835b242ff56
serpient <littlegangwolf@gmail.com>
2018-07-12 12:10:01 -0700

    add turtleTab feature data

--- src/TurtleTab/TurtleTab.jsx
+++ src/TurtleTab/TurtleTab.jsx
@@ -1,11 +1,41 @@
 import * as React from 'react';
-import './TurtleTab.css';
+import '../ProjectPageStyling/ProjectDetail.css';
+import { Link } from 'react-router-dom';
+import { projects } from '../utils/projects';
+import { skillCreator } from '../utils/skillCreator';
+import { projectFeatureCreator } from '../utils/projectFeatureCreator';
+
+var turtleTabData = projects[1];
+var features = [
+    {
+        image: require('../assets/turtleTab-search.png')
+    },
+    {
+        image: require('../assets/turtleTab-feature1.png')
+    },
+    {
+        image: require('../assets/turtleTab-feature2.png')
+    }
+]
 
 class TurtleTab extends React.Component {
     render() {
         return (
-            <div>
+            <div className="project-match-container">
+            <img
+                className="project-image project-image-padding"
+                src={require('../assets/turtleTab-main.png')}
+                alt="main-project"
+            />
+            <div className="project-page-title">{turtleTabData.name}</div>
+            <div className="project-page-description">{turtleTabData.description}</div>
+            <div className="project-page-tech">{skillCreator(turtleTabData.tech)}</div>
+            <div className="project-page-btns">
+                <Link to={turtleTabData.liveLink} className="main-navi-btn">Live Link</Link>
+                <Link to={turtleTabData.githubLink} className="main-navi-btn">Github Link</Link>
             </div>
+            {projectFeatureCreator(features)}
+        </div>
         )
     }
 }
