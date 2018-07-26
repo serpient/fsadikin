dbbb494be7f6799bb564edd84f7fdfca9f95cafa
serpient <littlegangwolf@gmail.com>
2018-07-26 16:11:09 -0700

    update work done in each project

--- src/utils/individualProjectPageCreator.js
+++ src/utils/individualProjectPageCreator.js
@@ -10,19 +10,31 @@ const removeRelativeLink = (string) => {
 export const individualProjectPageCreator = (projectData, mainImage) => {
     return (
         <div className="individual-project-container">
-        <img
-            className="project-image project-image-padding"
-            src={require('../assets/' + mainImage)}
-            alt="main-project"
-        />
-        <div className="project-page-title">{projectData.name}</div>
-        <div className="project-page-description">{projectData.description}</div>
-        <div className="project-page-tech">{skillCreator(projectData.tech)}</div>
-        <div className="project-page-btns">
-            <a href={removeRelativeLink(projectData.liveLink)} rel="external" target="_blank" className="main-navi-btn">Live Link</a>
-            <a href={removeRelativeLink(projectData.githubLink)} rel="external" target="_blank" className="main-navi-btn">Github Link</a>
+            <img
+                className="project-image project-image-padding"
+                src={require('../assets/' + mainImage)}
+                alt="main-project"
+            />
+            <div className="project-page-title">{projectData.name}</div>
+            <div className="project-page-description">{projectData.description}</div>
+            <div className="project-page-work">{
+                projectData.workDone.map((role, index) => {
+                    return (
+                    <div className="role-container">
+                        <div className="role" key={index}>
+                            <div className="role-dot"/> 
+                            {role}
+                        </div>
+                    </div>
+                    )
+                })
+            }</div>
+            <div className="project-page-tech">{skillCreator(projectData.tech)}</div>
+            <div className="project-page-btns">
+                <a href={removeRelativeLink(projectData.liveLink)} rel="external" target="_blank" className="main-navi-btn">Live Link</a>
+                <a href={removeRelativeLink(projectData.githubLink)} rel="external" target="_blank" className="main-navi-btn">Github Link</a>
+            </div>
+            {projectFeatureCreator(projectData.features)}
         </div>
-        {projectFeatureCreator(projectData.features)}
-    </div>
     )
 }
