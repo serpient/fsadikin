9cceaba37f31a4aa932b22c95e0475b30bb04d33
serpient <littlegangwolf@gmail.com>
2018-09-17 13:09:37 -0700

    add title to iframe

--- src/Resume/Resume.jsx
+++ src/Resume/Resume.jsx
@@ -5,7 +5,7 @@ class Resume extends React.Component {
     render() {
         return (
             <object className="resume-object" data={require('../assets/180914_Francesca Sadikin_Resume.pdf')} type="application/pdf">
-                <iframe className="resume-iframe" data={require('../assets/180914_Francesca Sadikin_Resume.pdf')}>
+                <iframe title="resume" className="resume-iframe" data={require('../assets/180914_Francesca Sadikin_Resume.pdf')}>
                     This browser does not support PDFs. Please download the PDF to view it:
                         <a href="https://drive.google.com/file/d/1R7iZWnmW7NXnlAHxMNBuQFt7584qowP3/view?usp=sharing">Download PDF</a>
                 </iframe>
