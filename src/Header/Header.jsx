07aa6faf860b0f30aa25ad8c62974577671df5fc
serpient <littlegangwolf@gmail.com>
2018-09-17 11:53:11 -0700

    add mobile header container

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -3,6 +3,12 @@ import { Link } from 'react-router-dom';
 import './Header.css';
 
 class Header extends React.Component {
+    state = {
+        dropdownVisibility: this.props.dropdown
+    }
+    toggleDropdown = () => {
+        this.setState({ dropdownVisibility: !this.state.dropdownVisibility })
+    }
     render() {
         return (
             <div className="header-container-all">
@@ -11,6 +17,15 @@ class Header extends React.Component {
                     <Link to="/about" className="navigation nav-about">About</Link>
                     <Link to="/projects" className="navigation nav-projects">Projects</Link>
                     <Link to="/resume" className="navigation nav-resume">Resume</Link>
+                </div>
+                <div className="header-container--mobile">
+                    <Link to="/" className="logo">Francesca Sadikin</Link>
+                    <img onClick={() => this.toggleDropdown} className="menu-icon" alt="menu" src={require('../assets/menu.png')} />
+                    <div className="menu">
+                        <Link to="/about" className="navigation">About</Link>
+                        <Link to="/projects" className="navigation">Projects</Link>
+                        <Link to="/resume" className="navigation">Resume</Link>
+                    </div>
                 </div>
                 <hr className="header-border" />
             </div>
