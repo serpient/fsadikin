32d87567301e047897e576bb8ccae4bd10e2318e
serpient <littlegangwolf@gmail.com>
2018-11-27 17:56:25 -0800

    try embedding google link in iframe

--- src/Resume/Resume.jsx
+++ src/Resume/Resume.jsx
@@ -2,13 +2,14 @@ import * as React from 'react';
 import './Resume.css';
 
 const resume = require('../assets/181127_Francesca Sadikin_Resume.pdf');
+const googleLinkToResume = `https://drive.google.com/file/d/1wX8sOqkmVvH8rAoIbLoYQjbjvjNUEJ_y/view?usp=sharing`;
 class Resume extends React.Component {
     render() {
         return (
             <object className="resume-object" data={resume} type="application/pdf">
-                <iframe title="resume" className="resume-iframe" src={resume}>
+                <iframe title="resume" className="resume-iframe" src={googleLinkToResume}>
                     This browser does not support PDFs. Please download the PDF to view it:
-                    <a href="https://drive.google.com/file/d/1wX8sOqkmVvH8rAoIbLoYQjbjvjNUEJ_y/view?usp=sharing">Download PDF</a>
+                    <a href={googleLinkToResume}>Download PDF</a>
                 </iframe>
             </object>
         )
