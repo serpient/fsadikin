3a278d113a8ad4dcf2510c9727b32927f3e0bd0b
serpient <littlegangwolf@gmail.com>
2018-11-27 17:25:06 -0800

    update resume

--- src/Resume/Resume.jsx
+++ src/Resume/Resume.jsx
@@ -1,13 +1,14 @@
 import * as React from 'react';
 import './Resume.css';
 
+const resume = require('../assets/181127_Francesca Sadikin_Resume.pdf');
 class Resume extends React.Component {
     render() {
         return (
-            <object className="resume-object" data={require('../assets/180914_Francesca Sadikin_Resume.pdf')} type="application/pdf">
-                <iframe title="resume" className="resume-iframe" data={require('../assets/180914_Francesca Sadikin_Resume.pdf')}>
+            <object className="resume-object" data={resume} type="application/pdf">
+                <iframe title="resume" className="resume-iframe" data={resume}>
                     This browser does not support PDFs. Please download the PDF to view it:
-                        <a href="https://drive.google.com/file/d/1R7iZWnmW7NXnlAHxMNBuQFt7584qowP3/view?usp=sharing">Download PDF</a>
+                        <a href="https://drive.google.com/file/d/1wX8sOqkmVvH8rAoIbLoYQjbjvjNUEJ_y/view?usp=sharing">Download PDF</a>
                 </iframe>
             </object>
         )
