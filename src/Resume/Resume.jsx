25b631e0805da6a0e926eeeb1e7b8086850875ef
serpient <littlegangwolf@gmail.com>
2018-11-27 17:59:27 -0800

    add embedded google link

--- src/Resume/Resume.jsx
+++ src/Resume/Resume.jsx
@@ -3,11 +3,12 @@ import './Resume.css';
 
 const resume = require('../assets/181127_Francesca Sadikin_Resume.pdf');
 const googleLinkToResume = `https://drive.google.com/file/d/1wX8sOqkmVvH8rAoIbLoYQjbjvjNUEJ_y/view?usp=sharing`;
+const embeddedGoogleLinkToResume = `https://drive.google.com/file/d/1wX8sOqkmVvH8rAoIbLoYQjbjvjNUEJ_y/preview`;
 class Resume extends React.Component {
     render() {
         return (
             <object className="resume-object" data={resume} type="application/pdf">
-                <iframe title="resume" className="resume-iframe" src={googleLinkToResume}>
+                <iframe title="resume" className="resume-iframe" src={embeddedGoogleLinkToResume}>
                     This browser does not support PDFs. Please download the PDF to view it:
                     <a href={googleLinkToResume}>Download PDF</a>
                 </iframe>
