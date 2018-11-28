25f6b5307361f8136ce77cc25b6baeff67d37f5e
serpient <littlegangwolf@gmail.com>
2018-11-27 17:50:29 -0800

    update iframe to src

--- src/Resume/Resume.jsx
+++ src/Resume/Resume.jsx
@@ -6,9 +6,9 @@ class Resume extends React.Component {
     render() {
         return (
             <object className="resume-object" data={resume} type="application/pdf">
-                <iframe title="resume" className="resume-iframe" data={resume}>
+                <iframe title="resume" className="resume-iframe" src={resume}>
                     This browser does not support PDFs. Please download the PDF to view it:
-                        <a href="https://drive.google.com/file/d/1wX8sOqkmVvH8rAoIbLoYQjbjvjNUEJ_y/view?usp=sharing">Download PDF</a>
+                    <a href="https://drive.google.com/file/d/1wX8sOqkmVvH8rAoIbLoYQjbjvjNUEJ_y/view?usp=sharing">Download PDF</a>
                 </iframe>
             </object>
         )
