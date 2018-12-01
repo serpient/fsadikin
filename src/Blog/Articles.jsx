799a8af3e29e14768cf18a7d9041fe49e5f0e1dc
serpient <littlegangwolf@gmail.com>
2018-11-30 18:24:05 -0800

    template article object schema

---
+++ src/Blog/Articles.jsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+const Article_1 = {
+  id: 1,
+  associatedProject: ``,
+  tags: [],
+  title: ``,
+  mainImage: ``,
+  article: (
+    <React.fragment>
+
+    </React.fragment>
+  ),
+}
