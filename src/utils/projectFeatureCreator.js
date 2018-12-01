dabffe54139d2d8f4e39fabe74ecfae37d21bd3f
serpient <littlegangwolf@gmail.com>
2018-11-30 19:42:17 -0800

    project creator renders image only if available

--- src/utils/projectFeatureCreator.js
+++ src/utils/projectFeatureCreator.js
@@ -6,20 +6,24 @@ export const projectFeatureCreator = (feature) => {
             <React.Fragment key={"feature" + index}>
                 <hr className="hline" />
                 <section className="project-page-feature">
-                    {feature.title ? <div className="project-page-feature-title">{feature.title}</div> : null }
+                    {feature.title ? <div className="project-page-feature-title">{feature.title}</div> : null}
                     {feature.description ? <div className="project-page-feature-description">{feature.description}</div> : null}
-                    <img
-                        className="project-image"
-                        src={feature.image}
-                        alt="project-feature"
-                    />
+                    {
+                        feature.image && (
+                            <img
+                                className="project-image"
+                                src={feature.image}
+                                alt="project-feature"
+                            />
+                        )
+                    }
                     {
                         feature.video && (
-                            <iframe 
+                            <iframe
                                 className="project-page-feature--video"
-                                src={feature.video} 
-                                frameborder="0" 
-                                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+                                src={feature.video}
+                                frameborder="0"
+                                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
                                 allowfullscreen
                             >
                             </iframe>
