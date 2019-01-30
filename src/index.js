0513d3e142b3f2665d1026f8568588fc8c3d0d8f
serpient <littlegangwolf@gmail.com>
2019-01-29 16:09:12 -0800

    wrap app with scrollToTop

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import ScrollToTop from './ScrollToTop';
 import { HashRouter } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<HashRouter><App /></HashRouter>, document.getElementById('root'));
+ReactDOM.render(
+  <HashRouter>
+    <ScrollToTop>
+      <App />
+    </ScrollToTop>
+  </HashRouter>, 
+  document.getElementById('root')
+);
 registerServiceWorker();
