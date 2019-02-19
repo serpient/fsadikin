4c5ecf848a6647279d3e41264f5f6d53f3c3a718
serpient <littlegangwolf@gmail.com>
2019-02-18 19:52:46 -0800

    move project-image; adjust styling for project feature creator

--- src/utils/projectFeatureCreator.js
+++ src/utils/projectFeatureCreator.js
@@ -7,34 +7,39 @@ export const projectFeatureCreator = (feature) => {
             <React.Fragment key={"feature" + index}>
                 <hr className="hline" />
                 <section className="project-page-feature">
-                    {feature.title ? <div className="project-page-feature-title">{feature.title}</div> : null}
-                    {feature.description 
-                        ?   <div className="project-page-feature-description">
-                                <ReactMarkdown source={feature.description} escapeHtml={false} />
-                            </div> 
-                        : null}
-                    {
-                        feature.image && (
-                            <img
-                                className="project-image"
-                                src={feature.image}
-                                alt="project-feature"
-                            />
-                        )
-                    }
-                    {
-                        feature.video && (
-                            <iframe
-                                className="project-page-feature--video"
-                                src={feature.video}
-                                frameborder="0"
-                                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                                allowfullscreen
-                                title="resume"
-                            >
-                            </iframe>
-                        )
-                    }
+                    <div className='project-page-info'>
+                        {feature.title ? <div className="project-page-feature-title">{feature.title}</div> : null}
+                        {feature.description 
+                            ?   <div className="project-page-feature-description">
+                                    <ReactMarkdown source={feature.description} escapeHtml={false} />
+                                </div> 
+                            : null}
+                    </div>
+                    <div className='project-page-media'>
+                        {
+                            feature.image && (
+                                <img
+                                    className="project-page-feature--image"
+                                    src={feature.image}
+                                    alt="project-feature"
+                                />
+                            )
+                        }
+                        {
+                            feature.video && (
+                                <iframe
+                                    className="project-page-feature--video"
+                                    src={feature.video}
+                                    frameborder="0"
+                                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                                    allowfullscreen
+                                    title="resume"
+                                >
+                                </iframe>
+                            )
+                        }
+                    </div>
+                    
                 </section>
             </React.Fragment>
         );
