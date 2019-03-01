eb6cce8ba911b6b9e69d4ef1d6be2d2f0e78465e
serpient <littlegangwolf@gmail.com>
2019-03-01 12:18:17 -0800

    change projects export to default

--- src/project_data/index.js
+++ src/project_data/index.js
@@ -5,7 +5,7 @@ import Form_Creator from "./Form_Creator";
 import Project_Match from "./Project_Match";
 import Turtles_Tab from "./Turtles_Tab";
 
-export const projects = new Map([
+const projects = new Map([
   ["chingu", Chingu],
   ["connect-4", Connect_4],
   ["book-finder", Book_Finder],
@@ -13,3 +13,5 @@ export const projects = new Map([
   ["project-match", Project_Match],
   ["turtle-tab", Turtles_Tab]
 ]);
+
+export default projects;
