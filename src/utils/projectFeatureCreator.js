fa0b7652df4e3e25966f68f54e2b0512bd7b1cbe
serpient <littlegangwolf@gmail.com>
2019-03-01 10:02:48 -0800

    prettier formatting

--- src/utils/projectFeatureCreator.js
+++ src/utils/projectFeatureCreator.js
@@ -1,42 +1,39 @@
-import * as React from 'react';
-const ReactMarkdown = require('react-markdown/with-html')
+import * as React from "react";
+const ReactMarkdown = require("react-markdown/with-html");
 
-export const projectFeatureCreator = (feature) => {
-    return feature.map((feature, index) => {
-        return (
-            <React.Fragment key={"feature" + index}>
-                <hr className="hline" />
-                <section className="project-page-feature">
-                    {feature.title ? <div className="project-page-feature-title">{feature.title}</div> : null}
-                    {feature.description 
-                        ?   <div className="project-page-feature-description">
-                                <ReactMarkdown source={feature.description} escapeHtml={false} />
-                            </div> 
-                        : null}
-                    {
-                        feature.image && (
-                            <img
-                                className="project-image"
-                                src={feature.image}
-                                alt="project-feature"
-                            />
-                        )
-                    }
-                    {
-                        feature.video && (
-                            <iframe
-                                className="project-page-feature--video"
-                                src={feature.video}
-                                frameborder="0"
-                                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                                allowfullscreen
-                                title="resume"
-                            >
-                            </iframe>
-                        )
-                    }
-                </section>
-            </React.Fragment>
-        );
-    })
-}
+export const projectFeatureCreator = feature => {
+  return feature.map((feature, index) => {
+    return (
+      <React.Fragment key={"feature" + index}>
+        <hr className="hline" />
+        <section className="project-page-feature">
+          {feature.title ? (
+            <div className="project-page-feature-title">{feature.title}</div>
+          ) : null}
+          {feature.description ? (
+            <div className="project-page-feature-description">
+              <ReactMarkdown source={feature.description} escapeHtml={false} />
+            </div>
+          ) : null}
+          {feature.image && (
+            <img
+              className="project-image"
+              src={feature.image}
+              alt="project-feature"
+            />
+          )}
+          {feature.video && (
+            <iframe
+              className="project-page-feature--video"
+              src={feature.video}
+              frameBorder="0"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+              title="resume"
+            />
+          )}
+        </section>
+      </React.Fragment>
+    );
+  });
+};
