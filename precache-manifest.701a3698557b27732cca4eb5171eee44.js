1fd862ce435d2004f9fa8f08752bfe4082f603f6
serpient <littlegangwolf@gmail.com>
2019-03-01 13:42:05 -0800

    Updates

---
+++ precache-manifest.701a3698557b27732cca4eb5171eee44.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = [
+  {
+    "revision": "12e91abb3ad168d2c741b92c4d5b5fc7",
+    "url": "/static/media/connect-4-short.12e91abb.gif"
+  },
+  {
+    "revision": "169457a3abb388fd44b8",
+    "url": "/static/css/main.0468c172.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "7504b2a6ad6af9ab322a",
+    "url": "/static/js/2.7504b2a6.chunk.js"
+  },
+  {
+    "revision": "0bf30d12eac1bfb81dd0683c86824421",
+    "url": "/static/media/profile.0bf30d12.jpg"
+  },
+  {
+    "revision": "e852a77e6873bf91fa31bb26ed1e65dc",
+    "url": "/static/media/book-finder_1000.e852a77e.png"
+  },
+  {
+    "revision": "f0aa91cbbe0d8cc279831a50426b2fb6",
+    "url": "/static/media/book-finder-main.f0aa91cb.png"
+  },
+  {
+    "revision": "d1823e8dfee06560667e9d105999ec15",
+    "url": "/static/media/book-finder.d1823e8d.gif"
+  },
+  {
+    "revision": "66836cbf7d4e970194731eac6e92b451",
+    "url": "/static/media/chingu.66836cbf.png"
+  },
+  {
+    "revision": "1bb9cfd59d3ae4aa36837cc1f465d9c4",
+    "url": "/static/media/chingu-landingImage.1bb9cfd5.JPG"
+  },
+  {
+    "revision": "1f3506d0ad91146b98a30b624b276906",
+    "url": "/static/media/Team-Sort-Features.1f3506d0.png"
+  },
+  {
+    "revision": "f6af4ba27b37e188c36339257da21009",
+    "url": "/static/media/chingu-voyage-application-2.f6af4ba2.png"
+  },
+  {
+    "revision": "ac7bcf8f0f0367ec1b92e712cb58b9d9",
+    "url": "/static/media/Chingu-Example NewsFeed.ac7bcf8f.png"
+  },
+  {
+    "revision": "433d88f26aba03c09d7cc1edf80f2d14",
+    "url": "/static/media/chingu-project-page.433d88f2.png"
+  },
+  {
+    "revision": "a60a15850f4bf06d5433760d696d644b",
+    "url": "/static/media/chingu-user-profile.a60a1585.png"
+  },
+  {
+    "revision": "d09e3ba17e76495b33a8d544074a5e02",
+    "url": "/static/media/ticketbox-01.d09e3ba1.png"
+  },
+  {
+    "revision": "626efb783df6377d594f03f2e16a4a53",
+    "url": "/static/media/connect-4_1000.626efb78.png"
+  },
+  {
+    "revision": "b1ccd003cd65e77da664132918b10f4e",
+    "url": "/static/media/connect-4-main.b1ccd003.png"
+  },
+  {
+    "revision": "169457a3abb388fd44b8",
+    "url": "/static/js/main.169457a3.chunk.js"
+  },
+  {
+    "revision": "551218368031fbc5f70d626c0dbd4ce6",
+    "url": "/static/media/form-creator_1000.55121836.png"
+  },
+  {
+    "revision": "e5d1f8e2ac502b3ad8505c9287edd6f1",
+    "url": "/static/media/form-creator.e5d1f8e2.gif"
+  },
+  {
+    "revision": "faa139f6528d81174929d44b9ed98522",
+    "url": "/static/media/form-creator-main.faa139f6.png"
+  },
+  {
+    "revision": "ebab4ea530b2bfe915ce627fb70a6edf",
+    "url": "/static/media/project-match.ebab4ea5.png"
+  },
+  {
+    "revision": "307312c4bd1e4dc96a595143c5399f74",
+    "url": "/static/media/project-match-main.307312c4.png"
+  },
+  {
+    "revision": "55c5c8cd69b7ea48764542ff43737292",
+    "url": "/static/media/project-match-project.55c5c8cd.png"
+  },
+  {
+    "revision": "d3282c3a98a35007bd85770efa56ad9b",
+    "url": "/static/media/project-match-project-portal.d3282c3a.png"
+  },
+  {
+    "revision": "194bf6b2a766ccc402b4367a1efa6a72",
+    "url": "/static/media/project-match-revision-Portal.194bf6b2.png"
+  },
+  {
+    "revision": "e2fcd900d16fa6544078d96dd241786b",
+    "url": "/static/media/project-match-user-profile.e2fcd900.png"
+  },
+  {
+    "revision": "7d59b0daafdf1d8a41212c72732f9b04",
+    "url": "/static/media/turtleTab.7d59b0da.png"
+  },
+  {
+    "revision": "fa3f0b94adde3f3d79c9238ac54a1ff0",
+    "url": "/static/media/turtleTab-main.fa3f0b94.png"
+  },
+  {
+    "revision": "c2f16f1dfc425c96782db30ef5bb45e1",
+    "url": "/static/media/turtleTab-search.c2f16f1d.png"
+  },
+  {
+    "revision": "ac6ca761a9f5286aafe5883f64021957",
+    "url": "/static/media/turtleTab-feature1.ac6ca761.png"
+  },
+  {
+    "revision": "f966a768a052a94e66fc831417dec566",
+    "url": "/static/media/turtleTab-feature2.f966a768.png"
+  },
+  {
+    "revision": "0d9c03b7b67b095924a86e7298febbd4",
+    "url": "/static/media/404-03.0d9c03b7.png"
+  },
+  {
+    "revision": "085192bf6df16eb7e7a004b1733f7515",
+    "url": "/static/media/francesca_resume_programming.085192bf.pdf"
+  },
+  {
+    "revision": "f5eb19249ee609c176671cd1a6b520f1",
+    "url": "/index.html"
+  }
+];
