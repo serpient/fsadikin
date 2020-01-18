ca319b6453174651cb52f12ad13a193d3f023dea
serpient <littlegangwolf@gmail.com>
2020-01-18 01:11:21 -0800

    Updates

---
+++ precache-manifest.204f90230f216497666929c7fa89251b.js
@@ -0,0 +1,214 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "020d423f3a22b3c9f1d1706d0e9add1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d52d11a3edb445a1a11",
+    "url": "/static/css/main.9d075f57.chunk.css"
+  },
+  {
+    "revision": "31a26d00f1773ae0bd45",
+    "url": "/static/js/2.6e2d5338.chunk.js"
+  },
+  {
+    "revision": "1b23ba65f027fa0b538ecc6055ad39ba",
+    "url": "/static/js/2.6e2d5338.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1d52d11a3edb445a1a11",
+    "url": "/static/js/main.7dea5e81.chunk.js"
+  },
+  {
+    "revision": "3465e3ebd04c87cf7fa5",
+    "url": "/static/js/runtime-main.1cbc98ff.js"
+  },
+  {
+    "revision": "0d9c03b7b67b095924a86e7298febbd4",
+    "url": "/static/media/404-03.0d9c03b7.png"
+  },
+  {
+    "revision": "ac7bcf8f0f0367ec1b92e712cb58b9d9",
+    "url": "/static/media/Chingu-Example NewsFeed.ac7bcf8f.png"
+  },
+  {
+    "revision": "1f3506d0ad91146b98a30b624b276906",
+    "url": "/static/media/Team-Sort-Features.1f3506d0.png"
+  },
+  {
+    "revision": "f0aa91cbbe0d8cc279831a50426b2fb6",
+    "url": "/static/media/book-finder-main.f0aa91cb.png"
+  },
+  {
+    "revision": "d1823e8dfee06560667e9d105999ec15",
+    "url": "/static/media/book-finder.d1823e8d.gif"
+  },
+  {
+    "revision": "e852a77e6873bf91fa31bb26ed1e65dc",
+    "url": "/static/media/book-finder_1000.e852a77e.png"
+  },
+  {
+    "revision": "1bb9cfd59d3ae4aa36837cc1f465d9c4",
+    "url": "/static/media/chingu-landingImage.1bb9cfd5.JPG"
+  },
+  {
+    "revision": "433d88f26aba03c09d7cc1edf80f2d14",
+    "url": "/static/media/chingu-project-page.433d88f2.png"
+  },
+  {
+    "revision": "a60a15850f4bf06d5433760d696d644b",
+    "url": "/static/media/chingu-user-profile.a60a1585.png"
+  },
+  {
+    "revision": "f6af4ba27b37e188c36339257da21009",
+    "url": "/static/media/chingu-voyage-application-2.f6af4ba2.png"
+  },
+  {
+    "revision": "66836cbf7d4e970194731eac6e92b451",
+    "url": "/static/media/chingu.66836cbf.png"
+  },
+  {
+    "revision": "b1ccd003cd65e77da664132918b10f4e",
+    "url": "/static/media/connect-4-main.b1ccd003.png"
+  },
+  {
+    "revision": "12e91abb3ad168d2c741b92c4d5b5fc7",
+    "url": "/static/media/connect-4-short.12e91abb.gif"
+  },
+  {
+    "revision": "626efb783df6377d594f03f2e16a4a53",
+    "url": "/static/media/connect-4_1000.626efb78.png"
+  },
+  {
+    "revision": "1b9f313bed114fffa6c8ecdf102a17e6",
+    "url": "/static/media/focal-client-page.1b9f313b.png"
+  },
+  {
+    "revision": "7306648e3b3159b0331b92bc5115a7cd",
+    "url": "/static/media/focal.7306648e.png"
+  },
+  {
+    "revision": "faa139f6528d81174929d44b9ed98522",
+    "url": "/static/media/form-creator-main.faa139f6.png"
+  },
+  {
+    "revision": "e5d1f8e2ac502b3ad8505c9287edd6f1",
+    "url": "/static/media/form-creator.e5d1f8e2.gif"
+  },
+  {
+    "revision": "551218368031fbc5f70d626c0dbd4ce6",
+    "url": "/static/media/form-creator_1000.55121836.png"
+  },
+  {
+    "revision": "085192bf6df16eb7e7a004b1733f7515",
+    "url": "/static/media/francesca_resume_programming.085192bf.pdf"
+  },
+  {
+    "revision": "da916b0978a3d57874296d8d7e8159d5",
+    "url": "/static/media/http-server.da916b09.png"
+  },
+  {
+    "revision": "4b25d616d11422d1847f309f385cc190",
+    "url": "/static/media/inclusive-maps-1.4b25d616.png"
+  },
+  {
+    "revision": "3225b8b71d5115511905ae59d7c8f779",
+    "url": "/static/media/inclusive-maps-3.3225b8b7.png"
+  },
+  {
+    "revision": "be2fc462255c35eeace21aa6c2b54114",
+    "url": "/static/media/inclusive-maps-4.be2fc462.png"
+  },
+  {
+    "revision": "5c13e723cf26d62e15088f6f1ab9edfa",
+    "url": "/static/media/inclusive-maps-main.5c13e723.png"
+  },
+  {
+    "revision": "df9a9b4dd31d5f38d5149bffef8a369e",
+    "url": "/static/media/inclusive-maps.df9a9b4d.png"
+  },
+  {
+    "revision": "307312c4bd1e4dc96a595143c5399f74",
+    "url": "/static/media/project-match-main.307312c4.png"
+  },
+  {
+    "revision": "d3282c3a98a35007bd85770efa56ad9b",
+    "url": "/static/media/project-match-project-portal.d3282c3a.png"
+  },
+  {
+    "revision": "55c5c8cd69b7ea48764542ff43737292",
+    "url": "/static/media/project-match-project.55c5c8cd.png"
+  },
+  {
+    "revision": "194bf6b2a766ccc402b4367a1efa6a72",
+    "url": "/static/media/project-match-revision-Portal.194bf6b2.png"
+  },
+  {
+    "revision": "e2fcd900d16fa6544078d96dd241786b",
+    "url": "/static/media/project-match-user-profile.e2fcd900.png"
+  },
+  {
+    "revision": "ebab4ea530b2bfe915ce627fb70a6edf",
+    "url": "/static/media/project-match.ebab4ea5.png"
+  },
+  {
+    "revision": "82315c1bced722ccf465319ad34a2b5f",
+    "url": "/static/media/rebay-auto-bid-feature.82315c1b.mp4"
+  },
+  {
+    "revision": "86e4fc3f005eb1277724375441637f99",
+    "url": "/static/media/rebay-item-auto-bid.86e4fc3f.png"
+  },
+  {
+    "revision": "cabee0061cd5f1175e38b2e81fa9ef88",
+    "url": "/static/media/rebay.cabee006.png"
+  },
+  {
+    "revision": "659aad64344ff147edadf6b3e78c148c",
+    "url": "/static/media/ruby_tic_tac_toe_lite_3.659aad64.gif"
+  },
+  {
+    "revision": "6de66077bfa1d5b1b62a3308cb8da527",
+    "url": "/static/media/ruby_tic_tac_toe_persistence.6de66077.gif"
+  },
+  {
+    "revision": "e686a0971cba399bb1867ed58617234b",
+    "url": "/static/media/ruby_tic_tac_toe_smart_computer.e686a097.gif"
+  },
+  {
+    "revision": "f6336e3c8b6eec52d163ebb582f42f74",
+    "url": "/static/media/tic-tac-toe.f6336e3c.png"
+  },
+  {
+    "revision": "d09e3ba17e76495b33a8d544074a5e02",
+    "url": "/static/media/ticketbox-01.d09e3ba1.png"
+  },
+  {
+    "revision": "2abd177c59e183b52fc11357c087c284",
+    "url": "/static/media/toggling_todo_from_table.2abd177c.mp4"
+  },
+  {
+    "revision": "ac6ca761a9f5286aafe5883f64021957",
+    "url": "/static/media/turtleTab-feature1.ac6ca761.png"
+  },
+  {
+    "revision": "f966a768a052a94e66fc831417dec566",
+    "url": "/static/media/turtleTab-feature2.f966a768.png"
+  },
+  {
+    "revision": "fa3f0b94adde3f3d79c9238ac54a1ff0",
+    "url": "/static/media/turtleTab-main.fa3f0b94.png"
+  },
+  {
+    "revision": "c2f16f1dfc425c96782db30ef5bb45e1",
+    "url": "/static/media/turtleTab-search.c2f16f1d.png"
+  },
+  {
+    "revision": "7d59b0daafdf1d8a41212c72732f9b04",
+    "url": "/static/media/turtleTab.7d59b0da.png"
+  },
+  {
+    "revision": "4b8aaeed5249baa382789a758bf5d8b7",
+    "url": "/static/media/update_package_and_create_event.4b8aaeed.mp4"
+  }
+]);
