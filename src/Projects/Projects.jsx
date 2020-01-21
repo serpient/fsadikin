55f699a7e9021f8973230295cae011265d853912
serpient <littlegangwolf@gmail.com>
2020-01-20 19:58:09 -0800

    Add timeline and grid mode to projects page

--- src/Projects/Projects.jsx
+++ src/Projects/Projects.jsx
@@ -1,22 +1,66 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Projects.scss';
 import projects from 'project_data';
 import ProjectCardCreator from './components/ProjectCardCreator';
 
-class Projects extends React.Component {
-  render() {
-    return (
-      <main className="projects-container">
-        <div className="projects-container-title">Francesca Sadikin</div>
-        <div className="projects-container-subtext">
-          Software Developer & UI/UX Designer
+const Projects = () => {
+  const [viewMode, setViewMode] = useState(
+    localStorage.getItem('projectViewMode') || 'timeline'
+  );
+  const [viewControlsIsVisible, setViewControlVisibility] = useState(false);
+
+  const getProjectClassName = viewMode => {
+    if (viewMode === 'timeline') {
+      return 'projects projects-border';
+    } else {
+      return 'projects-grid';
+    }
+  };
+
+  const getButtonClassName = (viewMode, match) => {
+    return `projects-view-mode-btn ${viewMode === match &&
+      'projects-view-mode-btn--active'}`;
+  };
+
+  useEffect(() => {
+    if (window.innerWidth < 500) {
+      setViewControlVisibility(false);
+    } else {
+      setViewControlVisibility(true);
+    }
+  }, [window.innerWidth]);
+
+  useEffect(() => {
+    localStorage.setItem('projectViewMode', viewMode);
+  }, [viewMode]);
+
+  return (
+    <main className="projects-container">
+      <div className="projects-container-title">Francesca Sadikin</div>
+      <div className="projects-container-subtext">
+        Software Developer & UI/UX Designer
+      </div>
+      {viewControlsIsVisible && (
+        <div className="project-view-mode-controller">
+          <button
+            className={getButtonClassName(viewMode, 'grid')}
+            onClick={() => setViewMode('grid')}
+          >
+            <i class="fas fa-th-large"></i>
+          </button>
+          <button
+            className={getButtonClassName(viewMode, 'timeline')}
+            onClick={() => setViewMode('timeline')}
+          >
+            <i class="fas fa-bars"></i>
+          </button>
         </div>
-        <section className="projects projects-border">
-          <ProjectCardCreator projects={projects} />
-        </section>
-      </main>
-    );
-  }
-}
+      )}
+      <section className={getProjectClassName(viewMode)}>
+        <ProjectCardCreator projects={projects} />
+      </section>
+    </main>
+  );
+};
 
 export default Projects;
--- src/Projects/components/ProjectCardCreator.jsx
+++ src/Projects/components/ProjectCardCreator.jsx
@@ -5,17 +5,9 @@ import SkillCreator from 'UI/SkillCreator';
 const ProjectCardCreator = ({ projects }) => {
   return Array.from(projects).map((project, index) => {
     project = project[1];
-    let {
-      date,
-      name,
-      tech,
-      description,
-      liveLink,
-      githubLink,
-      coverPhoto
-    } = project;
+    let { date, name, tech, description, coverPhoto } = project;
     return (
-      <React.Fragment key={'project_' + index}>
+      <section key={'project_' + index}>
         <div className="project-timeline-dot" />
         <div className="project-timeline-text">{date}</div>
         <div className="project">
@@ -25,30 +17,19 @@ const ProjectCardCreator = ({ projects }) => {
               <SkillCreator techArray={tech} />
             </div>
             <div className="project-description">{description}</div>
-            <div className="project-btns">
-              <Link
-                to={'/projects/' + name.replace(/ /g, '-')}
-                className="btn-more"
-              >
-                More Details
-              </Link>
-              {liveLink && (
-                <a href={liveLink} className="btn-more">
-                  Live Link
-                </a>
-              )}
-              <a href={githubLink} className="btn-more">
-                GitHub Link
-              </a>
-            </div>
           </div>
-          <img
+          <Link
+            to={'/projects/' + name.replace(/ /g, '-')}
             className="project-coverPhoto"
-            src={coverPhoto}
-            alt="coverPhoto"
-          />
+          >
+            <img
+              className="project-coverPhoto"
+              src={coverPhoto}
+              alt="coverPhoto"
+            />
+          </Link>
         </div>
-      </React.Fragment>
+      </section>
     );
   });
 };
